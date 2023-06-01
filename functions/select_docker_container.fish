57cf4eb4eeb5af498b435211d257c1903d8e6716
ikoba <i.koba.jp@gmail.com>
2023-06-02 02:15:10 +0900

    Expand only if the abbr is a prameter of a docker command

--- functions/select_docker_container.fish
+++ functions/select_docker_container.fish
@@ -1,3 +1,7 @@
 function select_docker_container
+  if not string match -q "docker*" (commandline)
+    return 1
+  end
+
   echo (docker ps -a | peco --initial-index 1 | awk '{print $1}' | tr "\r" " ")
 end
--- functions/select_docker_image.fish
+++ functions/select_docker_image.fish
@@ -1,3 +1,7 @@
 function select_docker_image
+  if not string match -q "docker*" (commandline)
+    return 1
+  end
+
   echo (docker images -a | peco --initial-index 1 | awk '{print $3}' | tr "\r" " ")
 end
