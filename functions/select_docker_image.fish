31476788e239723873a7e790e8b825c7a62a20e7
ikoba <i.koba.jp@gmail.com>
2023-06-02 02:02:14 +0900

    Append `-a` option

--- functions/select_docker_image.fish
+++ functions/select_docker_image.fish
@@ -1,3 +1,3 @@
 function select_docker_image
-  echo (docker images | peco --initial-index 1 | awk '{print $3}' | tr "\r" " ")
+  echo (docker images -a | peco --initial-index 1 | awk '{print $3}' | tr "\r" " ")
 end
